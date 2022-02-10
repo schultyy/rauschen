b9679b5773e0aeaba10c55f5ceee8e060bf5b4a9
Jan Schulte <janschulte@fastmail.com>
2022-02-09 23:26:00 -0500

    render gauge and sparkline

--- src/app/ui.rs
+++ src/app/ui.rs
@@ -2,7 +2,7 @@ use tui::backend::Backend;
 use tui::layout::{Alignment, Constraint, Direction, Layout, Rect};
 use tui::style::{Color, Style};
 use tui::text::{Span, Spans};
-use tui::widgets::{Block, BorderType, Borders, Cell, Paragraph, Row, Table, Sparkline};
+use tui::widgets::{Block, BorderType, Borders, Cell, Paragraph, Row, Table, Sparkline, Gauge};
 use tui::Frame;
 
 use super::actions::Actions;
@@ -22,41 +22,21 @@ where
         .constraints([Constraint::Length(3), Constraint::Min(10)].as_ref())
         .split(size);
 
-    // Title
-    let title = draw_title();
-    rect.render_widget(title, chunks[0]);
-
-    // Body & Help
-    let body_chunks = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([Constraint::Min(20), Constraint::Length(32)].as_ref())
-        .split(chunks[1]);
-
-    let body = draw_body(false, app.state());
-    rect.render_widget(body, body_chunks[0]);
+    let gauge = Gauge::default()
+        .block(Block::default().title("Volume").borders(Borders::ALL))
+        .gauge_style(Style::default().fg(Color::Yellow))
+        .percent(50);
+    rect.render_widget(gauge, chunks[0]);
 
     let sparkline = Sparkline::default()
         .block(
             Block::default()
-                .title("Data1")
+                .title("Playing")
                 .borders(Borders::LEFT | Borders::RIGHT),
         )
         .data(&app.state().sparkline_data().unwrap())
         .style(Style::default().fg(Color::Yellow));
-    rect.render_widget(sparkline, chunks[0]);
-
-}
-
-fn draw_title<'a>() -> Paragraph<'a> {
-    Paragraph::new("Plop with TUI")
-        .style(Style::default().fg(Color::LightCyan))
-        .alignment(Alignment::Center)
-        .block(
-            Block::default()
-                .borders(Borders::ALL)
-                .style(Style::default().fg(Color::White))
-                .border_type(BorderType::Plain),
-        )
+    rect.render_widget(sparkline, chunks[1]);
 }
 
 fn check_size(rect: &Rect) {
@@ -66,25 +46,4 @@ fn check_size(rect: &Rect) {
     if rect.height < 28 {
         panic!("Require height >= 28, (got {})", rect.height);
     }
-}
-
-fn draw_body<'a>(loading: bool, state: &AppState) -> Paragraph<'a> {
-    let loading_text = if loading { "Loading..." } else { "" };
-    let tick_text = if let Some(ticks) = state.count_tick() {
-        format!("Tick count: {}", ticks)
-    } else {
-        String::default()
-    };
-    Paragraph::new(vec![
-        Spans::from(Span::raw(loading_text)),
-        Spans::from(Span::raw(tick_text)),
-    ])
-    .style(Style::default().fg(Color::LightCyan))
-    .alignment(Alignment::Left)
-    .block(
-        Block::default()
-            .borders(Borders::ALL)
-            .style(Style::default().fg(Color::White))
-            .border_type(BorderType::Plain),
-    )
 }
