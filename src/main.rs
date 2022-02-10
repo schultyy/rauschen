d5af764065e480b8b1657113303ee24c7fcaa806
Jan Schulte <janschulte@fastmail.com>
2022-02-09 21:35:23 -0500

    Initial commit
    Playing sound

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::fs::File;
+use std::io::BufReader;
+use rodio::{Decoder, OutputStream, source::Source};
+
+fn main() {
+    // Get a output stream handle to the default physical sound device
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    // Load a sound from a file, using a path relative to Cargo.toml
+    let file = BufReader::new(File::open("resources/eurostar_car.wav").unwrap());
+    // Decode that sound file into a source
+    let source = Decoder::new(file).unwrap();
+    // Play the sound directly on the device
+    stream_handle.play_raw(source.convert_samples());
+
+    loop{}
+}
