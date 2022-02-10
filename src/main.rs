0037de85b7b5a356ee4d17eb02c6649e5f09819b
Jan Schulte <janschulte@fastmail.com>
2022-02-09 22:31:00 -0500

    add ui boilerplate
    using https://github.com/ilaborie/plop-tui/tree/blog/step-0 as a
    blueprint

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,56 @@
+use app::App;
 use rodio::Sink;
 use rodio::{Decoder, OutputStream};
+use std::cell::RefCell;
 use std::io::BufReader;
+use std::rc::Rc;
 use std::{error::Error, fs::File};
 
-fn main() -> Result<(), Box<dyn Error>> {
+use std::io::stdout;
+
+use tui::backend::CrosstermBackend;
+use tui::Terminal;
+
+use crate::app::ui;
+use eyre::Result;
+
+mod app;
+
+fn main() -> Result<()> {
+    let app = Rc::new(RefCell::new(App::new())); // TODO app is useless for now
+    start_ui(app)?;
+    Ok(())
+}
+
+
+#[allow(unreachable_code)]
+pub fn start_ui(app: Rc<RefCell<App>>) -> Result<()> {
+    // Configure Crossterm backend for tui
+    let stdout = stdout();
+    crossterm::terminal::enable_raw_mode()?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+    terminal.hide_cursor()?;
+
+    loop {
+        let app = app.borrow();
+
+        // Render
+        terminal.draw(|rect| ui::draw(rect, &app))?;
+
+        // TODO handle inputs here
+    }
+
+    // Restore the terminal and close application
+    terminal.clear()?;
+    terminal.show_cursor()?;
+    crossterm::terminal::disable_raw_mode()?;
+
+    Ok(())
+}
+
+fn playback() -> Result<(), Box<dyn Error>> {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 
     loop {
